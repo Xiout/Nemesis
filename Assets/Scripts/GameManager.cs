a65b670145789c73f6dc6aa5d3f9956cd50a4d26
Alexia Dauvergne <71349095+Xiout@users.noreply.github.com>
2022-03-02 23:31:35 +0100

    [WIP] Room attributions
    -Creation of a test scene with fewer rooms
    -Creation of GameManager, Room and Utilities Script
    -Implementation of random attribution for primary room only

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public enum RoomName{EVACUATION_SECTION_A, EVACUATION_SECTION_B, NEST, GENERATOR, STORAGE, LABORATORY, SURGERY, FIRE_SYSTEM, EMERGENCY}
+	
+	public List<Room> ListOfRoomPositions;
+
+	private RoomName[] _listOfRooms;
+	
+	#region GameManager
+	private static GameManager _instance = null;
+	public static GameManager Instance{
+		get{
+			if(_instance==null){
+				_instance = new GameManager();
+				//set all properties here
+				_instance.initializeRoomsPositions();
+			}
+			return _instance;
+		}
+	}
+	
+	private void Awake(){
+		_instance = this;
+		
+		if(_listOfRooms == null || _listOfRooms.Length == 0)
+			initializeRoomsPositions();
+	}
+	#endregion
+	
+	private void initializeRoomsPositions(){
+		List<RoomName> rooms = new List<RoomName>();
+		
+		for(int i=0;i<ListOfRoomPositions.Count; ++i){
+			rooms.Add((RoomName)i);
+		}
+		
+		rooms.Shuffle();
+		_listOfRooms = rooms.ToArray();
+	}
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+	//Properties relative to the room as a piece of the board
+	public bool IsExplored {get;set;}
+	public bool IsOnFire {get;set;}
+	public bool IsBroken {get;set;}
+	public int NumberItems {get;set;}
+	
+	//Properties elative to the type of room
+	public GameManager.RoomName roomName {get;private set;}
+	public string roomColor {get;private set;}
+	public bool hasComputer {get;private set;}
+	
+    // Start is called before the first frame update
+    private void Start()
+    {
+        IsExplored = false;
+		IsOnFire = false;
+		IsBroken = false;
+		NumberItems = 0;
+    }
+	
+	private void Explore(){
+	
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+	
+
+}
